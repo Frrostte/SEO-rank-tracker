eb4b7663a4e8fa474813bb73843ea5917cdcc7fa
Aditya <aditya@example.com>
2025-11-15 23:20:54 +0530

    change

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -4,14 +4,14 @@ import GoogleProvider from "next-auth/providers/google";
 import { MongoDBAdapter } from "@auth/mongodb-adapter"
 
 export const authOptions = {
-  providers: [
+  providers: [ 
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET
     })
   ],
   adapter: MongoDBAdapter(clientPromise),
-  secret: process.env.SECRET,
+  secret: process.env.NEXTAUTH_SECRET,
 };
 
 const handler = NextAuth(authOptions)
